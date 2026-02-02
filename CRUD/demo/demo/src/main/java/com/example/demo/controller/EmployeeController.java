17f53177c131cbfa578563fd139fa66ccaa9ca7c
Kyaw Zaw Hein <99558609+KZH2001@users.noreply.github.com>
2026-02-03 01:09:34 +0630

    Creating Update Employee REST API

--- CRUD/demo/demo/src/main/java/com/example/demo/controller/EmployeeController.java
+++ CRUD/demo/demo/src/main/java/com/example/demo/controller/EmployeeController.java
@@ -6,6 +6,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -40,6 +41,20 @@ public ResponseEntity<Employee> getEmployeeById(@PathVariable Long id) {
 	Employee employee = employeeRepository.findById(id)
 			.orElseThrow(() ->  new ResourceNotFoundException("Employee not exist id :" + id));
 	return ResponseEntity.ok(employee);
+}
+
+@PutMapping("/employee/{id}")
+public ResponseEntity<Employee> updateEmployee(@PathVariable Long id, @RequestBody Employee employeeDetails){
+	Employee employee = employeeRepository.findById(id)
+			.orElseThrow(() ->  new ResourceNotFoundException("Employee not exist id :" + id));
+	
+	employee.setFirstName(employeeDetails.getFirstName());
+	employee.setLastName(employeeDetails.getLastName());
+	employee.setEmailId(employeeDetails.getEmailId());
+	
+	Employee updatedEmployee = employeeRepository.save(employee);
+	return ResponseEntity.ok(updatedEmployee);
+
 }
 
 
